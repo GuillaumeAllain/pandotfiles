c58e1a6c7d9da6998b56715f781ba7ec057a6074
GuillaumeAllain <13963457+GuillaumeAllain@users.noreply.github.com>
2021-09-01 17:06:32 -0400

    corrected grep pattern and added math_spaces

--- src/pandotfiles/util/markdown.py
+++ src/pandotfiles/util/markdown.py
@@ -1,11 +1,18 @@
 from subprocess import run
+from xdg import xdg_data_home
 
 PANDOC_MARKDOWN_OUTPUT_COMMANDS = (
-    "-t markdown+yaml_metadata_block-grid_tables-simple_tables-multiline_tables"
+    "-t markdown+yaml_metadata_block-grid_tables-simple_tables-multiline_tables-latex_macros "
+    + '--data-dir="'
+    + str(xdg_data_home())
+    + '/pandot" -L math_spaces.lua'
     + " -s --markdown-headings=atx --wrap=preserve -V header-includes= -V include-before= -V include-after= "
 )
 PANDOC_MARKDOWN_OUTPUT_COMMANDS_NOYAML = (
-    "-t markdown+yaml_metadata_block-grid_tables-simple_tables-multiline_tables"
+    "-t markdown+yaml_metadata_block-grid_tables-simple_tables-multiline_tables-latex_macros"
+    + '--data-dir="'
+    + str(xdg_data_home())
+    + '/pandot" -L math_spaces.lua'
     + " --markdown-headings=atx --wrap=preserve -V header-includes= -V include-before= -V include-after= "
 )
 CLEAN_WHITESPACE = (
