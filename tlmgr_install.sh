6dc215c8c3739c8a6418c60c252a7420d75c8dc7
Guillaume Allain <13963457+GuillaumeAllain@users.noreply.github.com>
2024-09-15 20:56:12 -0400

    add tlmg file

---
+++ tlmgr_install.sh
@@ -0,0 +1,9 @@
+sudo tlmgr install latexmk lualatex-math
+sudo tlmgr install amsmath babel
+sudo tlmgr install selnolig siunitx glossaries mfirstuc xfor datatool tracklang blindtext
+sudo tlmgr install lastpage sectsty multibib ulthese pgf preprint cite tocloft glossaries-extra
+sudo tlmgr install francais-bst bib2gls pdfpages xpatch
+sudo tlmgr install fontaxes xstring silence newtx helvetic txfonts collection-fontsrecommended
+sudo tlmgr install lineno
+sudo luaotfload-tool --update
+
